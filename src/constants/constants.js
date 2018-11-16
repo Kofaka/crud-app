755a6849bb77d0006ed16b6389126c6ff4a3f489
Daria Babakova <kofaka@gmail.com>
2018-11-16 14:25:39 +0200

    Use constants from constants file.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,4 @@
 export const JSON_SERVER_URI = 'http://localhost:4000';
 export const API_KEY = '8E38EABE-532A-4269-AA76-120EB4BDBCD6';
 export const API_KEY2 = '55537FA1-D313-48DA-9E60-9135050457E5';
-export const CURRENCY = 'ETH/USD';
-// export const COINAPI_URI = `https://rest.coinapi.io/v1/exchangerate/${CURRENCY}?apikey=${API_KEY}`;
 export const COINAPI_URI = `https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_ETH_USD/history?apikey=${API_KEY}&period_id=1MIN&time_start=2018-01-01T00:00:00`;
--- src/modules/contracts/saga.js
+++ src/modules/contracts/saga.js
@@ -1,7 +1,7 @@
 import { put, call, takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
 
-import {JSON_SERVER_URI} from '../'
+import {JSON_SERVER_URI, COINAPI_URI} from '../../constants/constants'
 
 import {
   setContractsRequest,
@@ -10,18 +10,10 @@ import {
   fetchCoinApiRequest,
 } from './actions';
 
-const SERVER_URI = 'http://localhost:4000';
-const API_KEY = '8E38EABE-532A-4269-AA76-120EB4BDBCD6';
-const API_KEY2 = '55537FA1-D313-48DA-9E60-9135050457E5';
-const CURRENCY = 'ETH/USD';
-// const COINAPI_URI = `https://rest.coinapi.io/v1/exchangerate/${CURRENCY}?apikey=${API_KEY}`;
-const COINAPI_URI = `https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_ETH_USD/history?apikey=${API_KEY}&period_id=1MIN&time_start=2018-01-01T00:00:00`;
-
-
 function* requestContractsWatcher() {
   try {
     const data = yield call(() => {
-      return axios.get(`${SERVER_URI}/contracts`);
+      return axios.get(`${JSON_SERVER_URI}/contracts`);
     });
     yield put(fetchContractsRequest(data));
   } catch (error) {
