984921765f034ed804c3e78f20f1c93e10d9c944
Daria Babakova <kofaka@gmail.com>
2018-11-19 14:02:03 +0200

    Update source of db.json file depending of envonment.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,10 @@
-export const JSON_SERVER_URI = 'http://localhost:4000';
-export const CONTRACTS_DB_URI = '/db.json'; // In a production version tha call will be directly to db.
-                                            // The call to mocked DB is made to have ability
-                                            // to test this app on remote server without setting it
+// In a production version tha call will be directly to db.
+// The call to mocked DB is made to have ability to test this app on remote server without setting it
+export let CONTRACTS_DB_URI = '/db.json';
+if (process.env.NODE_ENV === 'production') {
+  CONTRACTS_DB_URI = '/crud-app/db.json';
+}
+
 export const API_KEY = '8E38EABE-532A-4269-AA76-120EB4BDBCD6';
 export const API_KEY2 = '55537FA1-D313-48DA-9E60-9135050457E5';
 export const COINAPI_URI = `https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_ETH_USD/history?apikey=${API_KEY}&period_id=1MIN&time_start=2018-01-01T00:00:00`;
