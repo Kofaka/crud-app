25a50bc79337afd8e2b2f796e385d6c9e9ba092f
Daria Babakova <kofaka@gmail.com>
2018-11-20 15:04:10 +0200

    Updates

---
+++ precache-manifest.ba54b18cc8abfc293ea19885bcc36905.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crud-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b9aec0977b946066a4fe",
+    "url": "/crud-app/static/js/runtime~main.b9aec097.js"
+  },
+  {
+    "revision": "4a35205ea9a0000e97af",
+    "url": "/crud-app/static/js/main.4a35205e.chunk.js"
+  },
+  {
+    "revision": "6658577e392bd8393e39",
+    "url": "/crud-app/static/js/1.6658577e.chunk.js"
+  },
+  {
+    "revision": "4a35205ea9a0000e97af",
+    "url": "/crud-app/static/css/main.8ce6809a.chunk.css"
+  },
+  {
+    "revision": "6658577e392bd8393e39",
+    "url": "/crud-app/static/css/1.0a838c2b.chunk.css"
+  },
+  {
+    "revision": "0d2025d9998809bd14c49d6ddd5340aa",
+    "url": "/crud-app/index.html"
+  }
+];
